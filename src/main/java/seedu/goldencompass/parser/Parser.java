64d6956ad72bccb1bfc1381a1ea972effc5232bc
uphyrdia <uphyrdia@gmail.com>
2026-03-11 15:23:00 +0800

    Add exception message

--- src/main/java/seedu/goldencompass/parser/Parser.java
+++ src/main/java/seedu/goldencompass/parser/Parser.java
@@ -1,12 +1,18 @@
 package seedu.goldencompass.parser;
 
+import seedu.goldencompass.exception.GoldenCompassException;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Parser {
 
     public String command = "";
     public String companyName = "";
     public String jobDescription = "";
 
-    public void parse(String message) throws IllegalArgumentException {
+    public void parse(String message) throws GoldenCompassException {
         String[] words = message.split("\\s+");
 
         command = words[0];
@@ -19,13 +25,14 @@ public class Parser {
                 try {
                     companyName = message.substring(5);
                 } catch (StringIndexOutOfBoundsException e) {
-                    throw new IllegalArgumentException("Find what?");
+                    throw new GoldenCompassException("Find what?");
                 }
             }
             default -> {
-                throw new IllegalArgumentException("Invalid command");
+                throw new GoldenCompassException("Invalid command");
             }
         }
-    }
 
+
+    }
 }
